c9d6fc9c6f75f09e1e036a78f981d0a3932274ba
Jonathan Contreras <jonathan.mcontreras@gmail.com>
2016-01-18 14:45:14 +0100

    Improve and split tests. Create test setUp for CPU ALU tests

--- Test/TestALU.cpp
+++ Test/TestALU.cpp
@@ -2,91 +2,130 @@
 #include "InstructionSet.h"
 #include "Types.h"
 
-TEST (CPU_ALU, increment16BitRegister) {
-    InstructionSet *set = new InstructionSet();
-    Register reg;
-    reg.value = 0x00;
+class CPU_ALU_TEST : public ::testing::Test {
+    protected:
+        InstructionSet* set;
+        Register reg;
+        byte flags;
     
+        CPU_ALU_TEST() {
+            set = new InstructionSet;
+        }
+    
+        virtual void SetUp() {
+            reg.value = 0x00;
+            flags = 0x00;
+        }
+    
+        ~CPU_ALU_TEST() {
+            delete set;
+        }
+};
+
+TEST_F (CPU_ALU_TEST, increment16BitRegister_happy) {
     set->increment16BitRegister(&reg);
     ASSERT_EQ(reg.value, 0x01);
-    
+}
+
+TEST_F (CPU_ALU_TEST, increment16BitRegister_half_carry) {
     reg.value = 0xFF;
     set->increment16BitRegister(&reg);
     ASSERT_EQ(reg.value, 0x100);
-    
+}
+
+TEST_F (CPU_ALU_TEST, increment16BitRegister_overflow) {
     reg.value = 0xFFFF;
     set->increment16BitRegister(&reg);
-    ASSERT_EQ(reg.value, 0x0000);
+    ASSERT_EQ(reg.value, 0x00);
 }
 
-TEST (CPU_ALU, increment8BitRegister) {
-    InstructionSet *set = new InstructionSet();
-    byte data = 0x00;
-    byte flags = 0x00;
-    
-    set->increment8BitRegister(&data, &flags);
-    ASSERT_EQ(data, 0x01);
+TEST_F (CPU_ALU_TEST, increment8BitRegister_happy) {
+    set->increment8BitRegister(&reg.hi, &flags);
+    ASSERT_EQ(reg.hi, 0x01);
     ASSERT_FALSE(isBitSet(flags, ZERO_FLAG));
     ASSERT_FALSE(isBitSet(flags, ADD_SUB_FLAG));
     ASSERT_FALSE(isBitSet(flags, HALF_CARRY_FLAG));
-    
-    data = 0x0F;
+}
+
+
+TEST_F (CPU_ALU_TEST, increment8BitRegister_half_carry) {
+    reg.hi = 0x0F;
     flags = 0xF0;
-    set->increment8BitRegister(&data, &flags);
-    ASSERT_EQ(data, 0x10);
+    set->increment8BitRegister(&reg.hi, &flags);
+    ASSERT_EQ(reg.hi, 0x10);
     ASSERT_FALSE(isBitSet(flags, ZERO_FLAG));
     ASSERT_FALSE(isBitSet(flags, ADD_SUB_FLAG));
     ASSERT_TRUE(isBitSet(flags, HALF_CARRY_FLAG));
     ASSERT_TRUE(isBitSet(flags, CARRY_FLAG));
-    
-    data = 0xFF;
+}
+
+TEST_F (CPU_ALU_TEST, increment8BitRegister_overflow_zero) {
+    reg.hi = 0xFF;
     flags = 0x00;
-    set->increment8BitRegister(&data, &flags);
-    ASSERT_EQ(data, 0x00);
+    set->increment8BitRegister(&reg.hi, &flags);
+    ASSERT_EQ(reg.hi, 0x00);
     ASSERT_TRUE(isBitSet(flags, ZERO_FLAG));
     ASSERT_FALSE(isBitSet(flags, ADD_SUB_FLAG));
     ASSERT_TRUE(isBitSet(flags, HALF_CARRY_FLAG));
 }
 
-TEST (CPU_ALU, decrement16BitRegister) {
-    InstructionSet *set = new InstructionSet();
-    Register data;
-    data.value = 0xFFFF;
-    
-    set->decrement16BitRegister(&data);
-    ASSERT_EQ(data.value, 0xFFFE);
-    
-    data.value = 0x0000;
-    set->decrement16BitRegister(&data);
-    ASSERT_EQ(data.value, 0xFFFF);
+
+TEST_F (CPU_ALU_TEST, decrement16BitRegister_happy) {
+    reg.value = 0xFFFF;
+    set->decrement16BitRegister(&reg);
+    ASSERT_EQ(reg.value, 0xFFFE);
 }
 
-TEST (CPU_ALU, decrement8BitRegister) {
-    InstructionSet *set = new InstructionSet();
-    
-    byte data = 0xFF;
-    byte flags = 0x00;
-    set->decrement8BitRegister(&data, &flags);
-    ASSERT_EQ(data, 0xFE);
+TEST_F (CPU_ALU_TEST, decrement16BitRegister_half_carry) {
+    reg.value = 0x0100;
+    set->decrement16BitRegister(&reg);
+    ASSERT_EQ(reg.value, 0x00FF);
+}
+
+TEST_F (CPU_ALU_TEST, decrement16BitRegister_overflow) {
+    reg.value = 0x0000;
+    set->decrement16BitRegister(&reg);
+    ASSERT_EQ(reg.value, 0xFFFF);
+}
+
+
+TEST_F (CPU_ALU_TEST, decrement8BitRegister_happy) {
+    reg.low = 0xFF;
+    set->decrement8BitRegister(&reg.low, &flags);
+    ASSERT_EQ(reg.low, 0xFE);
     ASSERT_FALSE(isBitSet(flags, ZERO_FLAG));
     ASSERT_TRUE(isBitSet(flags, ADD_SUB_FLAG));
     ASSERT_FALSE(isBitSet(flags, HALF_CARRY_FLAG));
-    
-    data = 0x01;
-    flags = 0xF0;
-    set->decrement8BitRegister(&data, &flags);
-    ASSERT_EQ(data, 0x00);
-    ASSERT_TRUE(isBitSet(flags, ZERO_FLAG));
-    ASSERT_TRUE(isBitSet(flags, ADD_SUB_FLAG));
-    ASSERT_FALSE(isBitSet(flags, HALF_CARRY_FLAG));
-    ASSERT_TRUE(isBitSet(flags, CARRY_FLAG));
-    
-    data = 0x10;
+}
+
+TEST_F (CPU_ALU_TEST, decrement8BitRegister_half_carry) {
+    reg.low = 0x10;
     flags = 0x00;
-    set->decrement8BitRegister(&data, &flags);
-    ASSERT_EQ(data, 0x0F);
+    set->decrement8BitRegister(&reg.low, &flags);
+    ASSERT_EQ(reg.low, 0x0F);
     ASSERT_FALSE(isBitSet(flags, ZERO_FLAG));
     ASSERT_TRUE(isBitSet(flags, ADD_SUB_FLAG));
     ASSERT_TRUE(isBitSet(flags, HALF_CARRY_FLAG));
 }
 
+TEST_F (CPU_ALU_TEST, decrement8BitRegister_zero) {
+    reg.hi = 0x01;
+    flags = 0xF0;
+    set->decrement8BitRegister(&reg.hi, &flags);
+    ASSERT_EQ(reg.hi, 0x00);
+    ASSERT_TRUE(isBitSet(flags, ZERO_FLAG));
+    ASSERT_TRUE(isBitSet(flags, ADD_SUB_FLAG));
+    ASSERT_FALSE(isBitSet(flags, HALF_CARRY_FLAG));
+    ASSERT_TRUE(isBitSet(flags, CARRY_FLAG));
+}
+
+TEST_F (CPU_ALU_TEST, decrement8BitRegister_overflow) {
+    reg.hi = 0x00;
+    flags = 0xF0;
+    set->decrement8BitRegister(&reg.hi, &flags);
+    ASSERT_EQ(reg.hi, 0xFF);
+    ASSERT_FALSE(isBitSet(flags, ZERO_FLAG));
+    ASSERT_TRUE(isBitSet(flags, ADD_SUB_FLAG));
+    ASSERT_TRUE(isBitSet(flags, HALF_CARRY_FLAG));
+    ASSERT_TRUE(isBitSet(flags, CARRY_FLAG));
+}
